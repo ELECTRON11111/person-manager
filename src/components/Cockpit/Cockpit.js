9f5fc7e1d9dfa6efca89a643d56c5b04ea94e203
daniel_eln <omoniyiopemipo6@gmail.com>
2022-09-24 15:26:49 +0100

    functional component update control using useEffect React Hook

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,31 +1,57 @@
-import React from "react";
+import React, {useEffect} from "react";
 
 const Cockpit = (props) => {
-    const classes = [];
-    if (props.length <= 2){
-      classes.push("red");
-    }
-    if (props.length <= 1){
-      classes.push("bold");
-    }
+  // useEffect react hook is used to control update with functional components
+  useEffect(() => {
+    // This is componentDidmount and componentDidUpdate in one effect
+    console.log('[App.js] useEffect');
+    // we can cause side effects, make http requests
+    // also useEffect can be used multiple times
+  });
 
-    if(props.showPersons) {
-      document.querySelector('.button').classList.add("personsShown")
-    } else {
-      document.querySelector('.button').classList.remove("personsShown")
-    }
+  useEffect(() => {
+    // to make this run once after page is rendered add an [] as a sencond arg to this fn
+    setTimeout(() => {
+      alert('[once] got data from server');
+    }, 1000);
+    // This will have similar effect as component did mount
+  }, [])
+  
+  useEffect(() => {
+    // to make this run once after an update is made 
+    // place the data to monitor, in the array
+    setTimeout(() => {
+      alert('[update] showPersons updated');
+    }, 1000)
+  }, [props.showPersons])
 
-    return (
-        <div>
-            <h1>{props.appTitle}</h1>
-            <p className= {classes.join(' ')}>this is really working.</p>
-            
-            <button
-            className='button'
-            onClick={props.clicked}
-            >Toggle Persons</button>
-        </div>
-    );
+  const classes = [];
+  if (props.length <= 2){
+    classes.push("red");
+  }
+  if (props.length <= 1){
+    classes.push("bold");
+  }
+
+
+  const btnClass = [];
+  if(props.showPersons) {
+    btnClass.push("personsShown");
+  } else {
+    btnClass.pop();
+  }
+
+  return (
+      <div>
+          <h1>{props.appTitle}</h1>
+          <p className= {classes.join(' ')}>this is really working.</p>
+          
+          <button
+          className= {`button ${btnClass.join('')}`}
+          onClick={props.clicked}
+          >Toggle Persons</button>
+      </div>
+  );
 }
 
 export default Cockpit;
